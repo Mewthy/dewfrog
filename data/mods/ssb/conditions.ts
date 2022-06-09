b1695c96287a1c6fc3a0d5ff605f04ac756fecc4
Zakuree <zach.hartin@gmail.com>
2022-06-08 22:43:04 -0400

    Update conditions.ts

--- data/mods/ssb/conditions.ts
+++ data/mods/ssb/conditions.ts
@@ -1,4 +1,39 @@
+import {FS} from '../../../lib';
+import {toID} from '../../../sim/dex-data';
+
+// Similar to User.usergroups. Cannot import here due to users.ts requiring Chat
+// This also acts as a cache, meaning ranks will only update when a hotpatch/restart occurs
+const usergroups: {[userid: string]: string} = {};
+const usergroupData = FS('config/usergroups.csv').readIfExistsSync().split('\n');
+for (const row of usergroupData) {
+	if (!toID(row)) continue;
+
+	const cells = row.split(',');
+	if (cells.length > 3) throw new Error(`Invalid entry when parsing usergroups.csv`);
+	usergroups[toID(cells[0])] = cells[1].trim() || ' ';
+}
+
+export function getName(name: string): string {
+	const userid = toID(name);
+	if (!userid) throw new Error('No/Invalid name passed to getSymbol');
+
+	const group = usergroups[userid] || ' ';
+	return group + name;
+}
+
 export const Conditions: {[k: string]: ModdedConditionData & {innateName?: string}} = {
+	journeyman: {
+		noCopy: true,
+		onStart() {
+			this.add(`c:|${Math.floor(Date.now() / 1000)}|${getName('Journeyman')}|pog?`);
+		},
+		onSwitchOut() {
+			this.add(`c:|${Math.floor(Date.now() / 1000)}|${getName('Journeyman')}|not pog`);
+		},
+		onFaint() {
+			this.add(`c:|${Math.floor(Date.now() / 1000)}|${getName('Journeyman')}|AAAAAAAAAAAAAAAAAAAAA`);
+		},
+	},
 	badtox: {
 		name: 'badtox',
 		effectType: 'Status',
